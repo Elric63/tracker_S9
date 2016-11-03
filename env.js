2149d306347e79d5ec2eb5984f50437792fae32a
alerousseau <alerousseau@enseirb-matmeca.fr>
2016-11-03 18:20:29 +0100

    Add env.js

---
+++ env.js
@@ -0,0 +1,5 @@
+/* Simple file to set environment variables to to share on gitHub, only use it locally */
+
+process.env['REDISURL'] = 'redis://127.0.0.1:6379';
+process.env['PORT'] = '6379';
+process.env['PORT_TRACKER'] = '6378';
