d7f2d81f8f7d813f8ff4ad4249e59a58160a9b8d
alerousseau <alerousseau@enseirb-matmeca.fr>
2016-11-07 09:05:16 +0100

    EDIT peer.js

--- app/tracker.js
+++ app/tracker.js
@@ -1,30 +1,39 @@
 var client = require('../app/redis');
 var config = require('../config');
 var express = require('express');
+var socket = require('../app/data/socket');
+var io = require('socket.io-client');
 
 
 var tracker = express();
-tracker.set('port', config.PORT);
+tracker.set('port', config.PORT_TRACKER);
 
 /* Setting the tracker server */
 
 console.log(tracker.get('port'));
 
-/*0
+
 var trackServer = tracker.listen(tracker.get('port'), function(){
     console.log('The Express tracker server is currently listening on port :' + trackServer.address().port
 
-
+    );
 });
-*/
+
 
 client.on('connect', function () {
     console.log('connected');
+
 
     client.set('monhash', 'Mon Message', function (err, reply) {
         console.log(reply);
     });
 });
+
+socket(trackServer, client);
+
+var yolo = io.connect('http://127.0.0.1:6378');
+
+
 
 
 
